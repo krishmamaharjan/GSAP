6f9d5efdea94abaab27f2427335cbfaa6a22c000
krishmamaharjan <krishmamaharjan73@gmail.com>
2025-10-07 17:46:56 +0545

    feat: hero image added with css

---
+++ src/components/Hero.jsx
@@ -0,0 +1,13 @@
+const Hero = () => {
+  return (
+    <div>
+        <section className="bg-black">
+            <div className="hero-container">
+                <img src="/images/hero-img.png" alt="hero-image" className="absolute bottom-0 left-1/2 -translate-x-1/2 object-auto scale-100 md:scale-150" />
+            </div>
+        </section>
+    </div>
+  )
+}
+
+export default Hero
