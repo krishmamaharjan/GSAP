f910a28f8063e45b70032138522d2808269dd694
krishmamaharjan <krishmamaharjan73@gmail.com>
2025-10-07 17:46:03 +0545

    feat: logo added in navbar

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,11 @@
+const Navbar = () => {
+  return (
+    <div>
+        <nav className='fixed top-0 left-0 z-50 p-3 md:p-9'>
+            <img src="/images/nav-logo.svg" alt="logo" className='w-20 md:w-24' />
+        </nav>
+    </div>
+  )
+}
+
+export default Navbar
